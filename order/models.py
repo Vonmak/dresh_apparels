8a9e9746c74fb08dd6bb82bef12fcd682a3c18eb
Victor Ondari <99796872+Vonmak@users.noreply.github.com>
2023-01-25 13:58:23 +0300

    update order user.

--- order/models.py
+++ order/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from accounts.models import Customer
+from accounts.models import User
 from product.models import Item
 
 # Create your models here.
 class OrderItem(models.Model) :
-    user = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
@@ -14,7 +14,7 @@ class OrderItem(models.Model) :
         return f"{self.quantity} of {self.item.item_name}"
     
 class Order(models.Model) :
-    user = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     items = models.ManyToManyField(OrderItem)
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
