dbdd4ec4ad09e517cad5aca18a130cfc2ce1230a
Victor Ondari <99796872+Vonmak@users.noreply.github.com>
2023-02-22 23:25:19 +0300

    update settings

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('', include(('app.urls','app'), namespace='app')),
+    path('api/', include(('api.urls','api'), namespace='api')),
 ]
