c17fafd08b7731e73f644865888c15e362361b44
Victor Ondari <99796872+Vonmak@users.noreply.github.com>
2023-02-22 23:20:53 +0300

    update product

--- product/forms.py
+++ product/forms.py
@@ -5,7 +5,6 @@ from .models import Item, Category
 
 
 class ProductForm(forms.ModelForm):
-    # item_category = forms.ChoiceField(Category, )
     class Meta:
         model = Item
         fields=['item_name','item_category','item_image','item_price','item_count', 'item_description']
--- product/models.py
+++ product/models.py
@@ -18,24 +18,20 @@ cat=[
         ('food','food'),
     ]
 class Category(models.Model):
-    # parent = models.ForeignKey('self', related_name='children', on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=255, choices=cat)
     slug = models.SlugField(max_length=255)
-    ordering = models.IntegerField(default=0)
-    is_featured = models.BooleanField(default=False)
 
     class Meta:
         verbose_name_plural = 'Categories'
-        ordering = ('ordering',)
 
     def __str__(self):
         return self.name
     
     def get_absolute_url(self):
         return '/%s/' % (self.name)
+    
 class Item(models.Model):
     user = models.ForeignKey(Merchant, on_delete=models.CASCADE, related_name="item")
-    # parent = models.ForeignKey('self', related_name='variants', on_delete=models.CASCADE, blank=True, null=True)
     item_name = models.CharField(max_length=50)
     item_slug = models.SlugField(max_length=255)
     item_description = models.TextField()
@@ -51,11 +47,6 @@ class Item(models.Model):
     def __str__(self):
         return self.item_name
     
-    # def get_absolute_url(self):
-    #     return reverse("app:product", kwargs={
-    #         'slug': self.slug
-    #     })
-
     def get_add_to_cart_url(self):
         return reverse("app:add-to-cart", kwargs={
             'slug': self.item_slug
