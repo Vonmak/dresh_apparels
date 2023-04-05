b9371c7312b95c1a38ce9dbe8ab49f5636c6c837
Guest <von.makori@gmail.com>
2023-04-05 12:35:07 +0300

    edited the admin and he styling in accounts.

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,6 +3,22 @@ from django.contrib import admin
 from .models import Customer, Merchant, User
 
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Merchant)
-admin.site.register(Customer)
+
+
+class UserA(admin.ModelAdmin):
+    list_display = ('email', 'id','first_name', 'last_name','phone','last_login','date_joined','is_customer','is_merchant','is_superuser',)
+    list_filter = ('last_login', 'is_customer', 'is_merchant', 'is_superuser', 'date_joined')
+    search_fields = ("first_name__startswith", )
+
+
+class CusA(admin.ModelAdmin):
+    list_display = ('user','id','date',)
+    list_filter = ('date',)    
+
+class MercA(admin.ModelAdmin):
+    list_display = ('user','id', 'till_no', 'location', 'date',)
+    list_filter = ('date',)
+        
+admin.site.register(User,UserA)
+admin.site.register(Merchant,MercA)
+admin.site.register(Customer, CusA)
--- accounts/views.py
+++ accounts/views.py
@@ -10,7 +10,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny
 
 from accounts.forms import *
 from .serializers import MerchantSerializer, CustomerSerializer, LoginSerializer
