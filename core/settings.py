d865c57e3866e0bf83748ec540dcab04e0687c2b
Guest <von.makori@gmail.com>
2023-07-25 16:50:40 +0300

    updated settings

--- core/settings.py
+++ core/settings.py
@@ -104,11 +104,11 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': '5432',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'IdpsjSHjQqsxKHcbEh5o',
+        'HOST': 'containers-us-west-206.railway.app',
+        'PORT': '6310',
     }
        
 }
