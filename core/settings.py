5816f3ede2bee966a4bd6498ef19465a9c355b05
Guest <von.makori@gmail.com>
2023-12-18 16:06:21 +0300

    minor updates

--- core/settings.py
+++ core/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-# import dj_database_url
-# from decouple import config,Csv
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
