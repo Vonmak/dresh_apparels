7f6b33a17c60ae50a06a56ffa086b0b72a3058fb
Guest <von.makori@gmail.com>
2024-04-13 13:34:58 +0300

    updates to the navbar and landing page

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from product.models import Category
+from product.models import Category, Item
 
 # Create your views here.
 def index(request):
     menu_categories = Category.objects.all()
+    products=Item.objects.all()
     return render(request,'index.html',locals())
