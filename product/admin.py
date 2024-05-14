7dee83647d5f668bdb0a5d7066ce930489158415
Guest <von.makori@gmail.com>
2024-05-14 16:40:44 +0300

    update filter for admin

--- product/admin.py
+++ product/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
+from .models import Category, Item
 
-from .models import *
-
-# Register your models here.
-class ItemA(admin.ModelAdmin):
-    list_display = ('item_name', 'item_description', 'item_price', 'item_count', 'date_added','user')
-    list_filter = ('date_added','item_price', 'item_count')
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('item_name', 'item_description', 'item_price', 'item_count', 'date_added', 'user')
+    list_filter = ('date_added', 'item_price', 'item_count','item_category')
     search_fields = ("item_name__startswith", )
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'parent_name')
+    prepopulated_fields = {'slug': ('name',)}
 
+    def parent_name(self, obj):
+        if obj.parent:
+            return obj.parent.name
+        else:
+            return '-'
 
-class CatA(admin.ModelAdmin):
-    list_display = ('name','slug')
-admin.site.register(Category, CatA)
-admin.site.register(Item, ItemA)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Item, ItemAdmin)
