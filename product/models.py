1940fa364466a1fd46afca40dbaa9d52118a5c6e
Guest <von.makori@gmail.com>
2024-05-22 16:12:05 +0300

    update models

--- product/models.py
+++ product/models.py
@@ -7,6 +7,9 @@ from cloudinary.models import CloudinaryField
 from accounts.models import Merchant
 from .slug import unique_slugify
 
+from decimal import Decimal
+from django.core.validators import MinValueValidator
+
 # Create your models here.
 cat = [
     ('vehicles', 'Vehicles'),
@@ -28,8 +31,13 @@ class Category(models.Model):
         unique_together = ('slug', 'parent',) 
         verbose_name_plural = 'Categories'
 
-    def __str__(self):
-        return self.name
+    def __str__(self):                           
+        full_path = [self.name]                  
+        k = self.parent
+        while k is not None:
+            full_path.append(k.name)
+            k = k.parent
+        return ' -> '.join(full_path[::-1]) 
     
     def get_absolute_url(self):
         return '/%s/' % (self.slug)
@@ -46,24 +54,15 @@ class Category(models.Model):
             child._get_descendants(descendants)
 
         return descendants
-
-    
-    def __str__(self):                           
-        full_path = [self.name]                  
-        k = self.parent
-        while k is not None:
-            full_path.append(k.name)
-            k = k.parent
-        return ' -> '.join(full_path[::-1]) 
     
 class Item(models.Model):
-    user = models.ForeignKey(Merchant, on_delete=models.CASCADE, related_name="item")
+    user = models.ForeignKey(Merchant, on_delete=models.CASCADE, related_name="items")
     item_name = models.CharField(max_length=50)
     item_slug = models.SlugField(max_length=255)
     item_description = models.TextField()
     item_image= CloudinaryField('image')
-    item_price = models.FloatField()
-    item_count = models.IntegerField()
+    item_price = models.DecimalField(max_digits=10, decimal_places=2)
+    item_count = models.IntegerField(validators=[MinValueValidator(0)])
     item_category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     date_added = models.DateTimeField(auto_now_add=True)
     
@@ -88,27 +87,25 @@ class Item(models.Model):
 
     def get_absolute_url(self):
         return reverse('app:product_detail', kwargs={'slug': self.item_slug})
-    
-    def save(self, **kwargs):
-        item_slug = '%s' % (self.item_name)
-        unique_slugify(self, item_slug)
-        super(Item, self).save()
-    
+
     def save(self, *args, **kwargs):
         if not self.item_slug:
-            self.item_slug = slugify(self.item_name)
-        super().save(*args, **kwargs)
-        
+            unique_slugify(self, self.item_name)
+        super(Item, self).save(*args, **kwargs)
+
+
     @classmethod
-    def filter_by_category(self, item):
-        cat = Item.objects.filter(item_category=item)
-        return cat
+    def filter_by_category(cls, category):
+        return cls.objects.filter(item_category=category)
+
+    @classmethod
+    def filter_by_owner(cls, owner):
+        return cls.objects.filter(user=owner)
+
     
     @classmethod
     def search(cls, query):
-        items = cls.objects.filter(
-            Q(item_category__name__icontains=query) |  # Search by category name
-            Q(item_name__icontains=query)  # Search by item name
+        return cls.objects.filter(
+            Q(item_category__name__icontains=query) |
+            Q(item_name__icontains=query)
         ).distinct()
-        
-        return items
